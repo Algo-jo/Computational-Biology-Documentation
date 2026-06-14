2a3c048cc334b33b17903566c01857518fa76436
Eren <aaronframon1@gmail.com>
2026-06-14 16:44:11 +0700

    Adding Documentation

---
+++ COMPBIO LAB SESI 4/practice.py
@@ -0,0 +1,87 @@
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# PREPROCESSING
+# Listing Image
+source_dir = f"(PATH)"
+source_images = [f for f in os.listdir(source_dir) if f.endswith(('.jpg', '.jpeg', '.png'))]
+
+# Loading Image
+target_raw = cv2.imread('f(PATH)')
+
+# Smoothing image
+def preprocess(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    equalizer = cv2.equalizeHist(gray)
+    blur = cv2.medianBlur(equalizer, 5)
+    return blur
+
+target_processed = preprocess(target_raw)
+
+# FEATURE DETECTION
+akaze = cv2.AKAZE_create()
+kp_target, desc_target = akaze.detectAndCompute(target_processed, None)
+if desc_target is not None:
+    if desc_target.dtype != np.float32:
+        desc_target = desc_target.astype(np.float32)
+
+best_score = -1
+best_match_data = None
+
+# FEATURE MATCHING
+for file in source_images:
+    file_raw = cv2.imread(os.path.join(source_dir, file))
+    file_processed = preprocess(file_raw)
+    
+    kp_file, desc_file = akaze.detectAndCompute(file_processed, None)
+    if desc_file is not None:
+        if desc_file.dtype != np.float32:
+            desc_file = desc_file.astype(np.float32)
+        
+        index_params = dict(algorithm=1, trees=5)
+        search_params = dict(check=50)
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+        matches = flann.knnMatch(desc_target, desc_file, k=2)
+        
+        good_match = []
+        for m, n in matches:
+            if m.distance < 0.7 * n.distance:
+                good_match.append([m])
+        
+        current_ratio = (len(good_match) / len(kp_target)) * 100 if len(kp_target) > 0 else 0
+        
+        if best_score < current_ratio:
+            best_score = current_ratio
+            best_match_data = {
+                'img': file_raw,
+                'kp': kp_file,
+                'name': file,
+                'matches': good_match,
+                'total_kp_target': len(kp_target)
+            }
+
+if best_match_data:
+    img_target_rgb = cv2.cvtColor(target_raw, cv2.COLOR_BGR2RGB)
+    img_rgb = cv2.cvtColor(target_raw, cv2.COLOR_BGR2RGB)
+    
+    img_res = cv2.drawMatchesKnn(
+        img_target_rgb, kp_target,\
+        img_rgb, best_match_data['kp'],
+        best_match_data['matches'],
+        None,
+        matchColor=[255,0,0],
+        singlePointColor=[0,0,255],
+        flags=cv2.DRAW_MATCHES_FLAGS_DEFAULT
+    )
+    plt.figure(figsize=(12,7))
+    plt.imshow(img_res)
+    plt.title(f"Best Match {best_match_data['name']} | Score: ({best_match_data['kp']} / {len(best_match_data['matches'])}) --- {best_score})")
+    
+    plt.axis('off')
+    plt.tight_layout()
+    plt.show()
+    
+else:
+    print("Gada")
